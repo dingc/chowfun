5f0606a528e54b18f8ad5d24db073b985d0341de
multizeroding@gmail.com <multizeroding@gmail.com>
2012-08-28 20:08:46 -0400

    clean

--- image/img_upload_form.php
+++
@@ -1,7 +0,0 @@
- <!--next comes the form, you must set the enctype to "multipart/frm-data" and use an input type "file" -->
- <form name="newad" method="post" enctype="multipart/form-data"  action="">
- <table>
- 	<tr><td><input type="file" name="image"></td></tr>
- 	<tr><td><input name="Submit" type="submit" value="Upload image"></td></tr>
- </table>	
- </form>
